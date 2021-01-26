fbe599a631ea714a1968cf2a8d352f211e05329d
chengzhe1 <76187232+chengzhe1@users.noreply.github.com>
2021-01-27 06:16:44 +0800

    Add files via upload

---
+++ Lab01.c
@@ -0,0 +1,9 @@
+﻿/*CS5008 Lab1 Assignment - Translate a python program to C*/
+
+int main() {
+	int term = spring2021;
+	int course = CS5008;
+	printf("I am now a member of" course, term, "!"); 
+	return 0;
+}
+
---
+++ Lab01.py
@@ -0,0 +1,5 @@
+def main():
+    term = 'spring2021'
+    course = 'CS5008'
+    print("I am now a member of", course, term, "!")
+main()
